2746a1f0fe7b6146dd2c911f7e4cec8bdbe4e9b9
mutuotakelvin <j17-3030-2019@student.mksu.ac.ke>
2022-01-01 12:23:52 +0300

    working with pipes

--- src/app/customers/shared/shared.module.ts
+++ src/app/customers/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 
+import { CapitalizePipe } from "./capitalized.pipes";
+
 @NgModule({
-    declarations: [],
-    exports: []
+    declarations: [CapitalizePipe],
+    exports: [CapitalizePipe]
 })
 
 export class SharedModule{}
