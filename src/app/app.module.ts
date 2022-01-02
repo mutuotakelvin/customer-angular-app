75513b08fe9a7d034437406f8301b337b9152de6
mutuotakelvin <j17-3030-2019@student.mksu.ac.ke>
2022-01-02 11:36:00 +0300

    refactoring [shared Module]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './customers/shared/shared.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
