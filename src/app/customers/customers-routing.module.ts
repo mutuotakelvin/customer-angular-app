7bf9d6834b847a24dacdafa345affcc8709da89c
mutuotakelvin <j17-3030-2019@student.mksu.ac.ke>
2022-01-07 14:58:45 +0300

    change routes

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CustomersComponent } from './customers.component';
 
 const routes: Routes = [
-  { path:'customers', component:CustomersComponent },
+  { path:'', component:CustomersComponent },
 ];
 
 @NgModule({
