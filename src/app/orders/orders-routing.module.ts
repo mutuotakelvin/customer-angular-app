9cd06065e9297ce0184ac9fd6a9bbe71160c9cd8
mutuotakelvin <j17-3030-2019@student.mksu.ac.ke>
2022-01-07 15:00:34 +0300

    change the path of the route

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { OrdersComponent } from '../orders/orders.component';
 
+
+
 const routes: Routes = [
-    {path:'orders/:id', component: OrdersComponent}
+    {path:':id', component: OrdersComponent}
 ];
 
 @NgModule({
