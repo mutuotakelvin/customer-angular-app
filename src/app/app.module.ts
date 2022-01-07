c1e01c37addcedc92549ed7b748d3aa360ada43b
mutuotakelvin <j17-3030-2019@student.mksu.ac.ke>
2022-01-07 14:51:48 +0300

    add OrdersModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-// import { OrdersModule } from './orders/orders.module';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     CustomersModule,
     SharedModule,
-    // OrdersModule
+    OrdersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
