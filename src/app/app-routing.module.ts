2664e9e0d5bd5ecef55fbe26b9746c5253084a54
mutuotakelvin <j17-3030-2019@student.mksu.ac.ke>
2022-01-07 14:32:10 +0300

    lazy load OrdersModule and CustomersModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', pathMatch: 'full', redirectTo: '/customers'},
-  {path: '**', pathMatch: 'full', redirectTo: '/customers'}
+  {
+    path: 'customers',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./customers/customers.module').then((cm) => cm.CustomersModule),
+  },
+  {
+    path: 'orders',
+    pathMatch: 'prefix',
+    loadChildren: () =>
+      import('./orders/orders.module').then((om) => om.OrdersModule),
+  },
+  { path: '**', pathMatch: 'full', redirectTo: '/customers' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: false,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
